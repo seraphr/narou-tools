b7ecafa2ee62fb50f36198b726e1b50b396abe4c
seraphr <seraph.sub+github@gmail.com>
2020-05-08 04:35:29 +0900

    ScatterChartExampleコンポーネントを分離＆antdのInputNumber使ってみた

--- narou-webui/src/main/scala/jp/seraphr/narou/webui/RootView.scala
+++ narou-webui/src/main/scala/jp/seraphr/narou/webui/RootView.scala
@@ -2,95 +2,43 @@ package jp.seraphr.narou.webui
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-import jp.seraphr.recharts.Tooltip.CursorStruct
-import jp.seraphr.recharts._
 
+import scala.annotation.nowarn
 import scala.scalajs.js
-import scala.util.Random
+import scala.scalajs.js.annotation.JSImport
+
+@JSImport("antd/dist/antd.css", JSImport.Default)
+@js.native
+object CSS extends js.Any
 
 object RootView {
+  @nowarn("cat=unused")
+  private val css = CSS
+
   case class Props(
     message: String
   )
 
-  trait XYZ extends js.Object {
-    val x: Int
-    val y: Int
-    val z: Int
-  }
+  case class State(dataCount: Int)
 
-  private def data(aX: Int, aY: Int, aZ: Int): XYZ = {
-    new XYZ {
-      override val x: Int = aX
-      override val y: Int = aY
-      override val z: Int = aZ
-    }
-  }
-
-  private val mRandom = new Random(10)
-  private def randomData(): XYZ = {
-    val x = mRandom.nextInt(10000)
-    val y = mRandom.nextInt(100000)
-    val z = mRandom.nextInt(500)
-    data(x, y, z)
-  }
-
-  private def randomDataSeq(aSize: Int): Seq[XYZ] = {
-    Seq.fill(aSize)(randomData())
-  }
-
-  val mData1 = Seq(
-    data(100, 200, 200),
-    data(120, 100, 260),
-    data(170, 300, 400),
-    data(140, 250, 280),
-    data(150, 400, 500),
-    data(110, 280, 200)
-  )
-  val mData2 = Seq(
-    data(200, 260, 240),
-    data(240, 290, 220),
-    data(190, 290, 250),
-    data(198, 250, 210),
-    data(180, 280, 260),
-    data(210, 220, 230)
-  )
-
-  def apply(message: String): ScalaFnComponent.Unmounted[Props] = component(Props(message))
+  def apply(message: String) = component(Props(message))
 
   val component =
-    ScalaFnComponent[Props] { props =>
-      println("======== createData")
-      val tRandomData1 = randomDataSeq(1000)
-      val tRandomData2 = randomDataSeq(1000)
-      println("======== createDom")
+    ScalaComponent.builder[Props]("RootView")
+      .initialState(State(dataCount = 100))
+      .renderPS { case (scope, p, s) =>
+        import typings.antd.components._
 
-      React.Fragment(
-        <.div(props.message),
-        ScatterChart(
-          ScatterChart.Props(
-            aWidth = 730,
-            aHeight = 250,
-            aMargin = Margin(
-              aTop = 20,
-              aRight = 20,
-              aBottom = 10,
-              aLeft = 10
-            )
-          )
-        )(
-            CartesianGrid(CartesianGrid.Props(Map(
-              "strokeDasharray" -> "3 3"
-            ))),
-            XAxis(XAxis.Props(aType = Axis.Type.number, aDataKey = "x", aName = "stature", aUnit = "cm")),
-            YAxis(YAxis.Props(aDataKey = "y", aName = "weight", aUnit = "kg")),
-            ZAxis(ZAxis.Props(aDataKey = "z", aRange = (10, 10), aName = "score", aUnit = "km")),
-            Tooltip(Tooltip.Props(aCursor = CursorStruct("3 3"))),
-            Legend(Legend.Props()),
-            Scatter(Scatter.Props(aName = "A school", aData = tRandomData1, aFill = "#8884d8"))(),
-            Scatter(Scatter.Props(aName = "B school", aData = tRandomData2, aFill = "#82ca9d"))()
-          )
-      )
-    }
+        val dataCountState = scope.mountedPure.zoomState(_.dataCount)(c => s => s.copy(dataCount = c))
+        React.Fragment(
+          <.div(p.message),
+          InputNumber(
+            defaultValue = s.dataCount,
+            onChange = { v => dataCountState.setStateOption(v.toOption.map(_.toInt)) }
+          )(),
+          ScatterChartExample(s.dataCount)
+        )
+      }
+      .build
 
 }
