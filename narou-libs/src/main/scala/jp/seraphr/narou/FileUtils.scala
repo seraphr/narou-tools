2a02f3ffe6b0814c925f3c620249b6ffe3e4553d
seraphr <seraph.sub+github@gmail.com>
2020-05-08 21:37:01 +0900

    NarouNovelsWriter を追加

---
+++ narou-libs/src/main/scala/jp/seraphr/narou/FileUtils.scala
@@ -0,0 +1,10 @@
+package jp.seraphr.narou
+
+import java.io.File
+
+object FileUtils {
+  implicit class FileOps(file: File) {
+    def /(aChild: String) = new File(file, aChild)
+    def modName(f: String => String) = new File(file.getParentFile, f(file.getName))
+  }
+}
