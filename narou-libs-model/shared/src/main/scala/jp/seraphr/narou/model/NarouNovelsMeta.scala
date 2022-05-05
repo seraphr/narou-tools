2ca2ecc4196401df7e3ae56a758de0f2139c969f
seraphr <seraph.sub+github@gmail.com>
2022-05-06 08:10:48 +0900

    ダミーデータでwebuiをプレビュー出来るようにした

--- narou-libs-model/shared/src/main/scala/jp/seraphr/narou/model/NarouNovelsMeta.scala
+++ narou-libs-model/shared/src/main/scala/jp/seraphr/narou/model/NarouNovelsMeta.scala
@@ -2,10 +2,22 @@ package jp.seraphr.narou.model
 
 import java.util.Date
 
+/**
+ *
+ * @param conditionDirs データが格納されているディレクトリ列
+ */
 case class ExtractedNarouNovelsMeta(
   conditionDirs: Seq[String]
 )
 
+/**
+ * [[ExtractedNarouNovelsMeta]]が保持する各ディレクトリに存在している小説情報のメタデータ
+ *
+ * @param name 人間可読な名前
+ * @param createdAt
+ * @param novelCount 何個の小説データが保持されているか
+ * @param novelFiles 小説情報を格納しているファイル名の列
+ */
 case class NarouNovelsMeta(
   name: String,
   createdAt: Date,
