72873a58855ce078106046bd45fe2e022d5f5ea7
seraphr <seraph.sub+github@gmail.com>
2022-05-07 05:46:11 +0900

    update scalacheck to 1.16.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
   object scalajs {
     val scalajsDom = Def.setting("org.scala-js" %%% "scalajs-dom" % "2.1.0")
     val scalatest = Def.setting("org.scalatest" %%% "scalatest" % scalatestVersion)
-    val scalacheck = Def.setting("org.scalatestplus" %%% "scalacheck-1-15" % scalatestplusVersion)
+    val scalacheck = Def.setting("org.scalatestplus" %%% "scalacheck-1-16" % scalatestplusVersion)
     val reactjs = Def.setting("com.github.japgolly.scalajs-react" %%% "core" % "2.1.0")
     val reactjsExtra = Def.setting("com.github.japgolly.scalajs-react" %%% "extra" % "2.1.0")
     val monixReactive = Def.setting("io.monix" %%% "monix-reactive" % monixVersion)
