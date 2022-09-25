d7e90c06ae9fcf9ffb4364af19397f7edb001f1c
seraphr <seraph.sub+github@gmail.com>
2022-09-26 05:43:20 +0900

    narou-libs-modelのjs側にDropboxNovelDataReaderを追加

---
+++ narou-libs-model/js/src/main/scala/jp/seraphr/narou/DropboxNovelDataReader.scala
@@ -0,0 +1,53 @@
+package jp.seraphr.narou
+
+import monix.eval.Task
+import monix.reactive.Observable
+import org.scalajs.dom.Blob
+import typings.dropbox.dropboxTypesMod.files.{DownloadArg, FileMetadata}
+import typings.dropbox.mod.{Dropbox, DropboxResponse}
+
+import scala.scalajs.js
+
+trait DropboxDownloadResponse extends DropboxResponse[FileMetadata] {
+  // see https://github.com/dropbox/dropbox-sdk-js/blob/dcc134dda8d39329c518ba66c2ac1ff187e43bde/src/response.js#L43-L64
+  
+  /** ブラウザ内の環境の時、こちらの値が入る */
+  var fileBlob: js.UndefOr[Blob]
+  
+  /** fileBlobが空の時、こちらに値が入る。 実際の型はnodejsのBuffer型。 bindingライブラリ入れてないのでjs.Anyにしておく */
+  var fileBinary: js.UndefOr[js.Any]
+}
+
+class DropboxNovelDataReader(aClient: Dropbox, aRootPath: String) extends NovelDataReader {
+  private val mRootPath = if (aRootPath.endsWith("/")) aRootPath.init else aRootPath
+
+  private def extractedMetadataPath = s"${mRootPath}/${NovelFileNames.extractedMetaFile}"
+  private def metadataPath(aDir: String) = s"${mRootPath}/${aDir}/${NovelFileNames.metaFile}"
+  private def novelPath(aDir: String, aFile: String) = s"${mRootPath}/${aDir}/${aFile}"
+  
+  private def downloadString(aPath: String): Task[String] = {
+    for {
+      tResponse <- Task.deferFuture(aClient.filesDownload(DownloadArg(aPath)).toFuture)
+      tDownloadResponse = tResponse.asInstanceOf[DropboxDownloadResponse]
+      tBlob = tDownloadResponse.fileBlob.getOrElse(throw new RuntimeException("fileBlobが見つかりません"))
+      tText <- Task.deferFuture(tBlob.text().toFuture)
+    } yield tText
+  }
+  
+  
+  override val extractedMeta: Task[String] = {
+    downloadString(extractedMetadataPath)
+  } 
+
+  override def metadata(aDir: String): Task[String] = {
+    downloadString(metadataPath(aDir))
+  }
+
+  override def getNovel(aDir: String, aFile: String): Observable[String] = {
+    // XXX Streaming処理したいがブラウザでreadlineするの地味に面倒そうなのでとりあえずやめた
+    for {
+      tText <- Observable.fromTask(downloadString(novelPath(aDir, aFile)))
+      tLine <- Observable.fromIterator(Task.eval(tText.linesIterator))
+    } yield tLine
+  }
+}
