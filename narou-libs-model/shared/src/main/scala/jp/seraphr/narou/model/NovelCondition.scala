c1353eb5c5743fc3f494788c6a49dd8aa44f1f91
seraphr <seraph.sub+github@gmail.com>
2023-04-17 01:44:28 +0900

    NovelConditionのparserを追加

--- narou-libs-model/shared/src/main/scala/jp/seraphr/narou/model/NovelCondition.scala
+++ narou-libs-model/shared/src/main/scala/jp/seraphr/narou/model/NovelCondition.scala
@@ -1,5 +1,10 @@
 package jp.seraphr.narou.model
 
+case class NovelConditionWithSource(condition: NovelCondition, source: String)        {
+  def id        = condition.id
+  def name      = condition.name
+  def predicate = condition.predicate
+}
 case class NovelCondition(id: String, name: String, predicate: NarouNovel => Boolean) {
   def and(aThat: NovelCondition): NovelCondition = {
     NovelCondition(
@@ -11,6 +16,14 @@ case class NovelCondition(id: String, name: String, predicate: NarouNovel => Boo
 
   def andQuietly(aThat: NovelCondition): NovelCondition = {
     this.copy(predicate = n => this.predicate(n) && aThat.predicate(n))
+  }
+
+  def or(aThat: NovelCondition): NovelCondition = {
+    NovelCondition(
+      s"${this.id}_or_${aThat.id}",
+      s"${this.name} | ${aThat.name}",
+      n => this.predicate(n) || aThat.predicate(n)
+    )
   }
 
   def not: NovelCondition = {
@@ -42,4 +55,126 @@ object NovelCondition {
   val bookmark500_1000: NovelCondition = bookmark(500, 1000)
   val bookmark1k_3k: NovelCondition    = bookmark(1000, 3000)
   val bookmark3k: NovelCondition       = bookmark(3000, 1000000)
+}
+
+object NovelConditionParser {
+  enum FilterOp {
+    case EQ, GE, GQ, LE, LQ
+
+    override def toString: String = this match {
+      case EQ => "=="
+      case GE => ">="
+      case GQ => ">"
+      case LE => "<="
+      case LQ => "<"
+    }
+
+  }
+
+  object FilterOp {
+    def fromString(str: String): FilterOp = str match {
+      case "==" => EQ
+      case ">=" => GE
+      case ">"  => GQ
+      case "<=" => LE
+      case "<"  => LQ
+    }
+
+  }
+
+  private def genFilter(toValue: NarouNovel => Int, op: FilterOp, value: Int): NarouNovel => Boolean = op match {
+    case FilterOp.EQ => toValue(_) == value
+    case FilterOp.GE => toValue(_) >= value
+    case FilterOp.GQ => toValue(_) > value
+    case FilterOp.LE => toValue(_) <= value
+    case FilterOp.LQ => toValue(_) < value
+  }
+
+  private type GenCondition = (FilterOp, Int) => NovelCondition
+  private val mFilterValues: Map[String, GenCondition] = Map(
+    "length"      -> { (op, value) =>
+      val tName   = s"length ${op} ${value}"
+      val tFilter = genFilter(_.length, op, value)
+
+      NovelCondition(tName, tName, tFilter)
+    },
+    "bookmark"    -> { (op, value) =>
+      val tName   = s"bookmark ${op} ${value}"
+      val tFilter = genFilter(_.bookmarkCount, op, value)
+
+      NovelCondition(tName, tName, tFilter)
+    },
+    "globalPoint" -> { (op, value) =>
+      val tName   = s"globalPoint ${op} ${value}"
+      val tFilter = genFilter(_.globalPoint, op, value)
+
+      NovelCondition(tName, tName, tFilter)
+    },
+    "evalPoint"   -> { (op, value) =>
+      val tName   = s"evalPoint ${op} ${value}"
+      val tFilter = genFilter(_.evaluationPoint, op, value)
+
+      NovelCondition(tName, tName, tFilter)
+    },
+    "evalCount"   -> { (op, value) =>
+      val tName   = s"evalCount ${op} ${value}"
+      val tFilter = genFilter(_.evaluationCount, op, value)
+
+      NovelCondition(tName, tName, tFilter)
+    }
+  )
+
+  private val mBooleanValues = Map(
+    "isFinished" -> NovelCondition.finished,
+    "isR15"      -> NovelCondition("isR15", "R15", _.isR15),
+    "isBL"       -> NovelCondition("isBL", "BL", _.isBL),
+    "isGL"       -> NovelCondition("isGL", "GL", _.isGL),
+    "isZankoku"  -> NovelCondition("isZankoku", "残酷", _.isZankoku),
+    "isTensei"   -> NovelCondition("isTensei", "転生", _.isTensei),
+    "isTenni"    -> NovelCondition("isTenni", "転移", _.isTenni)
+  )
+
+  import fastparse.*
+  import fastparse.SingleLineWhitespace.*
+
+  private def space[$: P]: P[Unit] = " ".rep
+
+  private def expr[$: P]: P[NovelCondition] = P(term ~ (space ~ ("&&" | "||").! ~ space ~ term).rep).map {
+    case (tHead, tTail) =>
+      tTail.foldLeft(tHead) { case (tLeft, (tOp, tRight)) =>
+        if (tOp == "&&") tLeft and tRight
+        else tLeft or tRight
+      }
+  }
+
+  private def term[S: P]: P[NovelCondition] = {
+    // mapして|で結合すると動かないが、foldLeftで結合すると動く。 何故かは知らん
+    def field[S: P]   = mFilterValues.keys.foldLeft[P[Unit]](Fail)(_ | P(_))
+    def op[S: P]      = StringIn("==", ">=", ">", "<=", "<")
+    def value[S: P]   = CharsWhile(_.isDigit, min = 1)
+    def tFilter[S: P] = P(field.! ~ op.! ~ value.!).map { (tField, tOp, tValue) =>
+      mFilterValues(tField)(FilterOp.fromString(tOp), tValue.toInt)
+    }
+
+    def tNot[S: P] = P("!" ~ factor).map(_.not)
+    tFilter | tNot | factor
+  }
+
+  private def factor[$: P]: P[NovelCondition] = {
+    def trueLiteral[$: P]  = P("true").map(_ => NovelCondition("true", "true", _ => true))
+    def falseLiteral[$: P] = P("false").map(_ => NovelCondition("false", "false", _ => false))
+    def tBoolean[S: P]     = P(mBooleanValues.keys.map(P(_).!).reduce(_ | _)).map(mBooleanValues(_))
+    def tParen[S: P]       = P("(" ~ expr ~ ")")
+
+    tBoolean | tParen | trueLiteral | falseLiteral
+  }
+
+  def apply(source: String): Either[String, NovelConditionWithSource] = {
+    def parser[$: P] = P(expr ~ End)
+    parse(source, parser(_)) match {
+      case Parsed.Success(tResult, _)  => Right(NovelConditionWithSource(tResult, source))
+      case Parsed.Failure(_, _, extra) => Left(extra.trace().longAggregateMsg)
+    }
+  }
+
 }
---
+++ narou-libs-model/shared/src/test/scala/jp/seraphr/narou/model/NovelConditionParserTest.scala
@@ -0,0 +1,263 @@
+package jp.seraphr.narou.model
+
+import monocle.Lens
+import org.scalacheck.Gen
+import org.scalatest.EitherValues
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class NovelConditionParserTest extends AnyFreeSpec with Matchers with EitherValues with ScalaCheckPropertyChecks {
+  val validUploadTypes = Seq(
+    UploadType.CellularPhone,
+    UploadType.PC,
+    UploadType.Both,
+    UploadType.Etc(1),
+    UploadType.Etc(2),
+    UploadType.Etc(3)
+  )
+
+  val narouNovelGen: Gen[NarouNovel] = for {
+    title             <- Gen.alphaNumStr
+    ncode             <- Gen.alphaNumStr
+    userId            <- Gen.alphaNumStr
+    writer            <- Gen.alphaNumStr
+    story             <- Gen.alphaNumStr
+    genreId           <- Gen.posNum[Int]
+    genreText         <- Gen.alphaNumStr
+    gensaku           <- Gen.alphaNumStr
+    keywords          <- Gen.listOf(Gen.alphaNumStr)
+    firstUpload       <- Gen.alphaNumStr
+    lastUpload        <- Gen.alphaNumStr
+    novelTypeId       <- Gen.oneOf(1, 2)
+    novelType         <- novelTypeId match {
+                           case 1 => Gen.const(NovelType.Serially)
+                           case 2 => Gen.const(NovelType.ShortStory)
+                         }
+    isFinished        <- Gen.oneOf(true, false)
+    chapterCount      <- Gen.posNum[Int]
+    length            <- Gen.posNum[Int]
+    readTimeMinutes   <- Gen.posNum[Int]
+    isR15             <- Gen.oneOf(true, false)
+    isBL              <- Gen.oneOf(true, false)
+    isGL              <- Gen.oneOf(true, false)
+    isZankoku         <- Gen.oneOf(true, false)
+    isTensei          <- Gen.oneOf(true, false)
+    isTenni           <- Gen.oneOf(true, false)
+    uploadType        <- Gen.oneOf(validUploadTypes)
+    globalPoint       <- Gen.posNum[Int]
+    bookmarkCount     <- Gen.posNum[Int]
+    reviewCount       <- Gen.posNum[Int]
+    evaluationPoint   <- Gen.posNum[Int]
+    evaluationCount   <- Gen.posNum[Int]
+    illustrationCount <- Gen.posNum[Int]
+    novelUpdatedAt    <- Gen.alphaNumStr
+    updatedAt         <- Gen.alphaNumStr
+  } yield NarouNovel(
+    title,
+    ncode,
+    userId,
+    writer,
+    story,
+    Genre(genreId, genreText),
+    gensaku,
+    keywords,
+    firstUpload,
+    lastUpload,
+    novelType,
+    isFinished,
+    chapterCount,
+    length,
+    readTimeMinutes,
+    isR15,
+    isBL,
+    isGL,
+    isZankoku,
+    isTensei,
+    isTenni,
+    uploadType,
+    globalPoint,
+    bookmarkCount,
+    reviewCount,
+    evaluationPoint,
+    evaluationCount,
+    illustrationCount,
+    novelUpdatedAt,
+    updatedAt
+  )
+
+  case class GenWithLens[A, B](gen: Gen[A], lens: Lens[A, B]) {
+    def by(b: B): Gen[A] = gen.map(lens.replace(b))
+  }
+
+  extension [A](aGen: Gen[A]) {
+    def replace[B](aLens: Lens[A, B]): GenWithLens[A, B] = GenWithLens(aGen, aLens)
+  }
+  import NarouNovel.lens
+
+  def assertParseSuccess(aSource: String, aTest: NarouNovel, aExpect: Boolean): Unit = {
+    assert(NovelConditionParser(aSource).value.predicate(aTest) === aExpect)
+    NovelConditionParser(aSource).value.source shouldBe aSource
+  }
+
+  "比較演算" - {
+    "length" - {
+      "==" - {
+        "match" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length == 1234", tNovel, true)
+          }
+        }
+
+        "notMatch" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length == 1235", tNovel, false)
+          }
+        }
+      }
+
+      "<" - {
+        "match" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length < 1235", tNovel, true)
+          }
+        }
+
+        "notMatch" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length < 1234", tNovel, false)
+          }
+        }
+      }
+
+      "<=" - {
+        "match" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length <= 1234", tNovel, true)
+          }
+        }
+
+        "notMatch" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length <= 1233", tNovel, false)
+          }
+        }
+      }
+
+      ">" - {
+        "match" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length > 1233", tNovel, true)
+          }
+        }
+
+        "notMatch" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length > 1234", tNovel, false)
+          }
+        }
+      }
+
+      ">=" - {
+        "match" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length >= 1234", tNovel, true)
+          }
+        }
+
+        "notMatch" in {
+          forAll(narouNovelGen replace lens.length by 1234) { tNovel =>
+            assertParseSuccess("length >= 1235", tNovel, false)
+          }
+        }
+      }
+    }
+  }
+
+  "リテラル" - {
+    "true" in {
+      forAll(narouNovelGen) { tNovel =>
+        assertParseSuccess("true", tNovel, true)
+      }
+    }
+
+    "false" in {
+      forAll(narouNovelGen) { tNovel =>
+        assertParseSuccess("false", tNovel, false)
+      }
+    }
+  }
+
+  "グルーピング" - {
+    "意味のない()" in {
+      forAll(narouNovelGen) { tNovel =>
+        assertParseSuccess("(true)", tNovel, true)
+      }
+    }
+
+    "(true || false) && (false ||  true) が 恒真となること" in {
+      forAll(narouNovelGen) { tNovel =>
+        assertParseSuccess("(true || false) && (false || true)", tNovel, true)
+      }
+    }
+  }
+
+  "結合演算子" - {
+    "&&" - {
+      "match" in {
+        forAll(narouNovelGen) { tNovel =>
+          assertParseSuccess("true && true", tNovel, true)
+        }
+      }
+
+      "notMatch" in {
+        forAll(narouNovelGen) { tNovel =>
+          assertParseSuccess("true && false", tNovel, false)
+          assertParseSuccess("false && true", tNovel, false)
+          assertParseSuccess("false && false", tNovel, false)
+        }
+      }
+    }
+
+    "||" - {
+      "match" in {
+        forAll(narouNovelGen) { tNovel =>
+          assertParseSuccess("true || true", tNovel, true)
+          assertParseSuccess("true || false", tNovel, true)
+          assertParseSuccess("false || true", tNovel, true)
+        }
+      }
+
+      "notMatch" in {
+        forAll(narouNovelGen) { tNovel =>
+          assertParseSuccess("false || false", tNovel, false)
+        }
+      }
+    }
+
+    "&&と||には結合順位が無く、左結合であること" in {
+      forAll(narouNovelGen) { tNovel =>
+        assertParseSuccess("false && false || true", tNovel, true)
+        assertParseSuccess("true || false && false", tNovel, false)
+      }
+    }
+  }
+
+  "否定" - {
+    "!" - {
+      "match" in {
+        forAll(narouNovelGen) { tNovel =>
+          assertParseSuccess("!false", tNovel, true)
+          assertParseSuccess("!(true && false)", tNovel, true)
+        }
+      }
+
+      "notMatch" in {
+        forAll(narouNovelGen) { tNovel =>
+          assertParseSuccess("!true", tNovel, false)
+          assertParseSuccess("!(true || false)", tNovel, false)
+        }
+      }
+    }
+  }
+}
