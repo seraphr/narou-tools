2b108ed7e5fb47c20d7c5b1c020ccff4feeee168
seraphr <seraph.sub+github@gmail.com>
2025-06-26 07:19:32 +0900

    sbt起動時のJVMオプションを調整

--- sbt.bat
+++ sbt.bat
@@ -1,3 +1,3 @@
-set SBT_OPTS=-Xmx1024M -Dinput.encoding=Cp1252 -Dfile.encoding=SJIS -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M
+set SBT_OPTS=-Xmx1024M -Dinput.encoding=Cp1252 -Dfile.encoding=SJIS -XX:MaxMetaspaceSize=1024M
 
 java %SBT_OPTS% -jar sbt-launch.jar %*
